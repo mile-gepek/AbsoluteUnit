a293f78114128dc048b06b134bd8057343636631
mile gepek <theontley@gmail.com>
2025-06-05 22:16:59 +0200

    Improved typing for the parsing module

--- absolute_unit/parsing.py
+++ absolute_unit/parsing.py
@@ -5,7 +5,8 @@ This module is used for parsing user input from commands
 import abc
 import enum
 import string
-from typing import Self, Generator
+from typing import Self, override
+from collections.abc import Generator
 
 __all__ = [
     "CharStream",
@@ -22,8 +23,8 @@ __all__ = [
 
 class CharStream:
     def __init__(self, string: str) -> None:
-        self._string = string
-        self._i = 0
+        self._string: str = string
+        self._i: int = 0
 
     @property
     def position(self) -> int:
@@ -53,13 +54,13 @@ class CharStream:
 
 
 class Token(abc.ABC):
-    total_alphabet = ""
+    total_alphabet: str = ""
 
     def __init__(self, stream: CharStream) -> None:
-        self._token = ""
-        self._start = stream.position
+        self._token: str = ""
+        self._start: int = stream.position
         self.consume(stream)
-        self._end = stream.position
+        self._end: int = stream.position
 
     @classmethod
     def from_stream(cls, stream: CharStream) -> Self | None:
@@ -125,10 +126,12 @@ class InvalidCharacter(Exception): ...
 
 
 class FloatToken(Token):
+    @override
     @staticmethod
     def default_alphabet() -> str:
         return string.digits + "."
 
+    @override
     def alphabet(self) -> str:
         if "." in self._token:
             return string.digits
@@ -139,6 +142,7 @@ class FloatToken(Token):
 
 
 class UnitToken(Token):
+    @override
     @staticmethod
     def default_alphabet() -> str:
         return string.ascii_letters
@@ -158,12 +162,14 @@ class ParenType(enum.Enum):
 class ParenToken(Token):
     def __init__(self, stream: CharStream) -> None:
         super().__init__(stream)
-        self._paren_type = ParenType(self._token)
+        self._paren_type: ParenType = ParenType(self._token)
 
+    @override
     @staticmethod
     def default_alphabet() -> str:
         return "()[]{}"
 
+    @override
     def alphabet(self) -> str:
         if self._token:
             return ""
@@ -185,12 +191,14 @@ class OperatorType(enum.Enum):
 class OperatorToken(Token):
     def __init__(self, stream: CharStream) -> None:
         super().__init__(stream)
-        self._op_type = OperatorType(self._token)
+        self._op_type: OperatorType = OperatorType(self._token)
 
+    @override
     @staticmethod
     def default_alphabet() -> str:
         return "+-*/"
 
+    @override
     def alphabet(self) -> str:
         if not self._token:
             return self.default_alphabet()
@@ -204,16 +212,19 @@ class OperatorToken(Token):
 
 
 class WhitespaceToken(Token):
+    @override
     @staticmethod
     def default_alphabet() -> str:
         return string.whitespace
 
 
 class UnknownToken(Token):
+    @override
     @staticmethod
     def default_alphabet() -> str | None:
         return None
 
+    @override
     def consume(self, stream: CharStream):
         while char := stream.peek():
             if char in Token.total_alphabet:
@@ -226,7 +237,7 @@ class UnknownToken(Token):
 def tokenize(s: str) -> Generator[Token, None, None]:
     stream = CharStream(s)
     while stream:
-        token = Token.from_stream(stream)
+        token: Token | None = Token.from_stream(stream)
         if token is None:
             break
         if not isinstance(token, WhitespaceToken):
