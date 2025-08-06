136e94eca641650bfcc52429cd64491999b6defc
mile gepek <theontley@gmail.com>
2025-08-06 15:21:37 +0200

    Simple manual testing setup.

--- absolute_unit/__main__.py
+++ absolute_unit/__main__.py
@@ -1,6 +1,36 @@
-from .parsing import parse
+from result import Err
+from .parsing import parse, ParsingErrorGroup, _EOF
 
 if __name__ == "__main__":
-    text = "3.6 km 14m + [(57 / 27)] ** 3"
-    parsed = parse(text)
-    print(parsed)
+    inputs = [
+        "3.6 * km + 14 * m + [(57 * inch / 27)] ** 1",
+        "4.5 + -3.6",
+    ]
+    while inp := input().rstrip():
+        stripped = inp.strip()
+        leading_whitespace = len(inp) - len(stripped)
+        parsed = parse(stripped)
+        if isinstance(parsed, Err):
+            errors = ParsingErrorGroup()
+            errors.add(parsed.err_value)
+            for exc in errors.errors:
+                print(" " * leading_whitespace, end="")
+                span = exc.span
+                if not isinstance(span, _EOF):
+                    length = span[1] - span[0]
+                    print(" " * span[0], end="")
+                    print("^" * length, end="")
+                    print(" " * (len(stripped) - span[1] + 4), end="")
+                    print(exc)
+                else:
+                    print(" " * len(stripped), end="")
+                    print(" ^", end="")
+                    print(" ", exc)
+            continue
+        value = parsed.ok_value
+        human_readable = str(value)
+        print(human_readable)
+        print("=")
+        evaluated_str = str(value.evaluate())
+        print(evaluated_str)
+        print("-" * len(evaluated_str))
