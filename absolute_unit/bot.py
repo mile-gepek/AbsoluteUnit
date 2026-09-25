d8350ca42ad12521e9691255d6f74ab98477e188
theontley <theontley@gmail.com>
2026-09-25 18:28:18 +0200

    Refactored bot to use common unit registry in conversion module

--- absolute_unit/bot.py
+++ absolute_unit/bot.py
@@ -85,7 +85,7 @@ class Bot(commands.InteractionBot):
         config = Config.get_config().unwrap()
         setup_logging(config.log_level)
         client = commands.InteractionBot(test_guilds=config.test_guild_ids)
-        ureg = UnitRegistry("units.txt", autoconvert_offset_to_baseunit=True)
+        ureg = conversion.get_unit_registry()
         return cls(settings, config, client, ureg)
 
 
